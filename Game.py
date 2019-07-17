c394884768757e7cd215097a7099f57e17b36d25
Serin Delaunay <serin.delaunay@gmail.com>
2019-07-17 22:09:33 +0100

    step 3: opponent

--- Game.py
+++ Game.py
@@ -5,11 +5,15 @@ import Token
 
 class Game:
     def __init__(self):
-        self.health_points = [Token.HealthToken for _ in range(10)]
+        self.health_points_left = [Token.HealthToken for _ in range(10)]
+        self.health_points_right = [Token.HealthToken for _ in range(10)]
         self.stop = True
+        self.width = 80
+        self.height = 50
 
     def run(self):
         blt.open()
+        blt.set(f"window.size={self.width}x{self.height}")
         self.stop = False
         while not self.stop:
             self.draw()
@@ -18,10 +22,15 @@ class Game:
         pass
 
     def draw(self):
+        assert self.width % 2 == 0, "Window size not divisible by 2"
         blt.clear()
-        for i, s in enumerate(self.health_points):
-            blt.printf(0, i + 2, s.name)
+        for i, s in enumerate(self.health_points_left):
+            blt.puts(0, i + 2, s.name, width=self.width//2, height=1, align=blt.TK_ALIGN_LEFT)
             blt.put(i * 2, 0, s.attack.symbol)
+
+        for i, s in enumerate(self.health_points_right):
+            blt.puts(self.width//2, i + 2, s.name, width=self.width//2, height=1, align=blt.TK_ALIGN_RIGHT)
+            blt.put(self.width - 1 - (i * 2), 0, s.attack.symbol)
         blt.refresh()
 
     def read(self):
