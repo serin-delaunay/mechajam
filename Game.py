78958ad30e7897a0079c8aa5a23bc22b29f04fcf
Serin Delaunay <serin.delaunay@gmail.com>
2019-08-05 14:54:16 +0100

    Refactor draw function

--- Game.py
+++ Game.py
@@ -56,23 +56,29 @@ class Game:
 
     def inactive_player(self):
         return self.right_player if self.next_turn_left else self.left_player
-
+        
+    def draw_tokens(self, player, align_right=False):
+        left = self.width // 2 if align_right else 0
+        align = blt.TK_ALIGN_RIGHT if align_right else blt.TK_ALIGN_LEFT
+        for i, line in enumerate(player.display_lines()):
+            blt.puts(left, i + 2, line, width=self.width // 2, height=1, align=align)
+            
+    def draw_attacks(self, player, align_right=False):
+        for (i, a) in enumerate(player.get_attacks()):
+            x = self.width - 1 - (i * 2) if align_right else (i * 2)
+            blt.put(x, 0, a.symbol)
+        
     def draw(self):
         assert self.width % 2 == 0, "Window size not divisible by 2"
         blt.clear()
         self.left_player.active = self.next_turn_left
         self.right_player.active = not self.next_turn_left
-        for i, line in enumerate(self.left_player.display_lines()):
-            blt.puts(0, i + 2, line, width=self.width // 2, height=1, align=blt.TK_ALIGN_LEFT)
-        attacks = self.left_player.get_attacks()
-        for (i, a) in enumerate(attacks):
-            blt.put(i * 2, 0, a.symbol)
+        
+        self.draw_tokens(self.left_player)
+        self.draw_attacks(self.left_player)
+        self.draw_tokens(self.right_player, True)
+        self.draw_attacks(self.right_player, True)
 
-        for i, line in enumerate(self.right_player.display_lines()):
-            blt.puts(self.width // 2, i + 2, line, width=self.width // 2, height=1, align=blt.TK_ALIGN_RIGHT)
-        attacks = self.right_player.get_attacks()
-        for (i, a) in enumerate(attacks):
-            blt.put(self.width - 1 - (i * 2), 0, a.symbol)
         blt.refresh()
 
     def read(self):
